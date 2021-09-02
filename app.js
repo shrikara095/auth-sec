153e7882537f12e869bfa36f36e39f0db996bb8e
shrikara <hshrikara@gmail.com>
2021-09-02 20:17:53 +0530

    level 1

---
+++ app.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+
+const express=require("express");
+const bodyparser=require("body-parser");
+const ejs=require("ejs");
+const app=express();
+const mongoose=require("mongoose");
+const encrypt=require("mongoose-encryption")
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyparser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+const userSchema= new mongoose.Schema({
+	email:String,
+	password:String
+});
+
+const secret="Thisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
+const User=new mongoose.model("User",userSchema);
+
+
+app.get("/",function(req,res){
+	res.render("home");
+})
+app.get("/login",function(req,res){
+	res.render("login");
+	})
+
+app.post("/login",function(req,res){
+
+	const username=req.body.username;
+	const password=req.body.password;
+	User.findOne({email:username},function(err,foundUser){
+		if(err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			if(foundUser)
+			{
+				if(foundUser.password===password)
+				{
+					res.render("secrets")
+				}
+			}
+		}
+	})
+
+})
+app.get("/register",function(req,res){
+	res.render("register")
+
+})
+
+app.post("/register",function(req,res){
+	const newUser=new User({
+		email:req.body.username,
+		password:req.body.password
+
+	});
+	newUser.save(function(err){
+		if (err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			res.render("secrets")
+		}
+	})
+})
+app.listen(3000,function(){
+	console.log("server started on port 3000");
+})
